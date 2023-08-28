78fa377d66a161eda663b13b07cbf401ad844477
Lalit <39011029+Strangersknowme@users.noreply.github.com>
2023-08-28 13:00:04 +0530

    Update index.js

--- example/src/Camera/index.js
+++ example/src/Camera/index.js
@@ -17,6 +17,43 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
 function __makeTemplateObject(cooked, raw) {
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
     return cooked;
@@ -53,6 +90,8 @@ var Camera = React.forwardRef(function (_a, ref) {
     var _k = useState(facingMode), currentFacingMode = _k[0], setFacingMode = _k[1];
     var _l = useState(false), notSupported = _l[0], setNotSupported = _l[1];
     var _m = useState(false), permissionDenied = _m[0], setPermissionDenied = _m[1];
+    var _o = useState(false), torchsupported = _o[0], setTorchSupported = _o[1];
+    var _p = useState(false), isTorchOn = _p[0], setTorchOnOff = _p[1];
     useEffect(function () {
         numberOfCamerasCallback(numberOfCameras);
     }, [numberOfCameras]);
@@ -109,10 +148,26 @@ var Camera = React.forwardRef(function (_a, ref) {
         getNumberOfCameras: function () {
             return numberOfCameras;
         },
+        toggleTorch: function () {
+            var currentTrack = (stream === null || stream === void 0 ? void 0 : stream.getVideoTracks()[0]) || (stream === null || stream === void 0 ? void 0 : stream.getTracks()[0]);
+            if (torchsupported) {
+                var torchValue = isTorchOn ? false : true;
+                currentTrack === null || currentTrack === void 0 ? void 0 : currentTrack.applyConstraints({
+                    advanced: [{ torch: torchValue }],
+                });
+                setTorchOnOff(torchValue);
+            }
+            else {
+                console.log("Torch not supported");
+            }
+        },
+        flashStatus: function () {
+            return torchsupported ? true : false;
+        }
     }); });
     useEffect(function () {
-        initCameraStream(stream, setStream, currentFacingMode, videoSourceDeviceId, setNumberOfCameras, setNotSupported, setPermissionDenied);
-    }, [currentFacingMode, videoSourceDeviceId]);
+        initCameraStream(stream, setStream, currentFacingMode, videoSourceDeviceId, setNumberOfCameras, setNotSupported, setPermissionDenied, setTorchSupported, setTorchOnOff);
+    }, [currentFacingMode, setTorchSupported, setTorchOnOff, videoSourceDeviceId]);
     useEffect(function () {
         if (stream && player && player.current) {
             player.current.srcObject = stream;
@@ -135,7 +190,7 @@ var Camera = React.forwardRef(function (_a, ref) {
             React.createElement(Canvas, { ref: canvas }))));
 });
 Camera.displayName = 'Camera';
-var initCameraStream = function (stream, setStream, currentFacingMode, videoSourceDeviceId, setNumberOfCameras, setNotSupported, setPermissionDenied) {
+var initCameraStream = function (stream, setStream, currentFacingMode, videoSourceDeviceId, setNumberOfCameras, setNotSupported, setPermissionDenied, setTorchSupported, setTorchOnOff) {
     var _a;
     // stop any active streams in the window
     if (stream) {
@@ -148,30 +203,52 @@ var initCameraStream = function (stream, setStream, currentFacingMode, videoSour
         video: {
             deviceId: videoSourceDeviceId ? { exact: videoSourceDeviceId } : undefined,
             facingMode: currentFacingMode,
-            width: { ideal: 1920 },
-            height: { ideal: 1920 },
+            width: { ideal: 1080 },
+            height: { ideal: 1080 },
         },
     };
     if ((_a = navigator === null || navigator === void 0 ? void 0 : navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia) {
         navigator.mediaDevices
             .getUserMedia(constraints)
-            .then(function (stream) {
-            setStream(handleSuccess(stream, setNumberOfCameras));
-        })
+            .then(function (stream) { return __awaiter(void 0, void 0, void 0, function () {
+            var result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, checkTorchExists(navigator === null || navigator === void 0 ? void 0 : navigator.mediaDevices, stream)];
+                    case 1:
+                        result = _a.sent();
+                        setTorchSupported(result);
+                        setTorchOnOff(result);
+                        setStream(handleSuccess(stream, setNumberOfCameras));
+                        return [2 /*return*/];
+                }
+            });
+        }); })
             .catch(function (err) {
             handleError(err, setNotSupported, setPermissionDenied);
         });
     }
     else {
-        var getWebcam = navigator.getUserMedia ||
+        var getWebcam_1 = navigator.getUserMedia ||
             navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia ||
             navigator.mozGetUserMedia ||
             navigator.msGetUserMedia;
-        if (getWebcam) {
-            getWebcam(constraints, function (stream) {
-                setStream(handleSuccess(stream, setNumberOfCameras));
-            }, function (err) {
+        if (getWebcam_1) {
+            getWebcam_1(constraints, function (stream) { return __awaiter(void 0, void 0, void 0, function () {
+                var result;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, checkTorchExists(getWebcam_1, stream)];
+                        case 1:
+                            result = _a.sent();
+                            setTorchSupported(result);
+                            setTorchOnOff(result);
+                            setStream(handleSuccess(stream, setNumberOfCameras));
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, function (err) {
                 handleError(err, setNotSupported, setPermissionDenied);
             });
         }
@@ -186,6 +263,35 @@ var handleSuccess = function (stream, setNumberOfCameras) {
         .then(function (r) { return setNumberOfCameras(r.filter(function (i) { return i.kind === 'videoinput'; }).length); });
     return stream;
 };
+function checkTorchExists(device, stream) {
+    return __awaiter(this, void 0, void 0, function () {
+        var supportedConstraints, track, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    supportedConstraints = device === null || device === void 0 ? void 0 : device.getSupportedConstraints();
+                    track = stream.getTracks()[0];
+                    if (!(supportedConstraints && 'torch' in supportedConstraints && track && track.applyConstraints)) return [3 /*break*/, 5];
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, track.applyConstraints({ advanced: [{ torch: true }] })];
+                case 2:
+                    _a.sent();
+                    return [2 /*return*/, true];
+                case 3:
+                    error_1 = _a.sent();
+                    console.log("Torch constraint is not supported.");
+                    return [2 /*return*/, false];
+                case 4: return [3 /*break*/, 6];
+                case 5:
+                    console.log("Torch constraint is not supported.");
+                    return [2 /*return*/, false];
+                case 6: return [2 /*return*/];
+            }
+        });
+    });
+}
 var handleError = function (error, setNotSupported, setPermissionDenied) {
     console.error(error);
     //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
