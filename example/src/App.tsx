51a9a3e5cc8a6df1b80007cdeeabda1c4bc3557a
Lalit <39011029+Strangersknowme@users.noreply.github.com>
2023-08-28 13:00:30 +0530

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -154,7 +154,7 @@ const App = () => {
       ) : (
         <Camera
           ref={camera}
-          aspectRatio="cover"
+          aspectRatio={4 / 3}
           numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
           videoSourceDeviceId={activeDeviceId}
           errorMessages={{
@@ -168,6 +168,28 @@ const App = () => {
             console.log('Video feed ready.');
           }}
         />
+      )}
+
+      {camera?.current?.flashStatus() ? (
+        <button  onClick={()=>{
+          if (camera?.current) {
+            camera.current.toggleTorch();
+          }}} 
+          id="torchbutton">
+          {/*Lightning Icon*/}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={20}
+            height={20}
+            className="bi bi-lightning-charge"
+            viewBox="0 0 16 16"
+            fill="#00000"
+          >
+            <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41 4.157 8.5z" />
+          </svg>
+        </button>
+      ) : (
+        ''
       )}
       <Control>
         <select
